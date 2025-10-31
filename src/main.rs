10d40e082ba092772c36f90ca97525494f98092f
John T. Wodder II <git@varonathe.org>
2025-10-31 17:38:06 -0400

    Define a dedicated error type

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use anyhow::Context as _;
 use std::ffi::OsString;
 use std::future::Future;
 use std::io::{self, Write};
 use std::pin::{pin, Pin};
-use std::process::{ExitCode, Stdio};
+use std::process::{ExitCode, ExitStatus, Stdio};
 use std::task::{ready, Context, Poll};
 use std::time::{Duration, Instant};
+use thiserror::Error;
 use tokio::{
     io::{AsyncRead, ReadBuf},
     process::Command,
@@ -32,29 +32,19 @@ fn main() -> ExitCode {
         .kill_on_drop(true);
     match run(cmd) {
         Ok(code) => code,
+        Err(e) if e.is_epipe_write() => ExitCode::SUCCESS,
         Err(e) => {
-            // Unlike most uses of this idiom, don't go through e.chain()
-            // looking for a BrokenPipe, as we only want to check errors raised
-            // by failure to write to stdout/stderr, which are those for which
-            // the payload is io::Error.  If we were to go through e.chain(),
-            // then we might get false positives if reading from the child
-            // process failed with EPIPE (Can that even happen?  Not sure.)
-            if let Some(ioerr) = e.downcast_ref::<io::Error>() {
-                if ioerr.kind() == io::ErrorKind::BrokenPipe {
-                    return ExitCode::SUCCESS;
-                }
-            }
-            let _ = writeln!(io::stderr().lock(), "{argv0}: {e:?}");
+            let _ = writeln!(io::stderr().lock(), "{argv0}: {e}");
             ExitCode::FAILURE
         }
     }
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn run(mut cmd: Command) -> anyhow::Result<ExitCode> {
+async fn run(mut cmd: Command) -> Result<ExitCode, Error> {
     let statline = StatusLine::new();
     let mut ticker = interval(Duration::from_secs(1));
-    let mut p = cmd.spawn().context("failed to start command")?;
+    let mut p = cmd.spawn().map_err(Error::Spawn)?;
     let mut stdout = ByteLines::new(p.stdout.take().expect("Child.stdout should be Some"));
     let mut stderr = ByteLines::new(p.stderr.take().expect("Child.stderr should be Some"));
     statline.print()?;
@@ -66,42 +56,24 @@ async fn run(mut cmd: Command) -> anyhow::Result<ExitCode> {
             },
             r = stdout.next_line() => {
                 statline.clear()?;
-                match r {
-                    Ok(line) => {
-                        io::stdout().lock().write_all(&line)?;
-                        statline.print()?;
-                    }
-                    Err(e) => {
-                        return Err(e).context("error reading from child process's stdout");
-                    }
-                }
+                let line = r.map_err(Error::ReadStdout)?;
+                io::stdout().lock().write_all(&line).map_err(Error::Write)?;
+                statline.print()?;
             }
             r = stderr.next_line() => {
                 statline.clear()?;
-                match r {
-                    Ok(line) => {
-                        io::stderr().lock().write_all(&line)?;
-                        statline.print()?;
-                    }
-                    Err(e) => {
-                        return Err(e).context("error reading from child process's stderr");
-                    }
-                }
+                let line = r.map_err(Error::ReadStderr)?;
+                io::stderr().lock().write_all(&line).map_err(Error::Write)?;
+                statline.print()?;
             }
             r = p.wait() => {
                 statline.clear()?;
-                match r {
-                    Ok(rc) => {
-                        if let Some(ret) = rc.code() {
-                            let ret = u8::try_from(ret & 255).unwrap_or(1);
-                            return Ok(ExitCode::from(ret));
-                        } else {
-                            anyhow::bail!("Child process killed by signal: {rc}");
-                        }
-                    }
-                    Err(e) => {
-                        return Err(e).context("error waiting for child process to terminate");
-                    }
+                let rc = r.map_err(Error::Wait)?;
+                if let Some(ret) = rc.code() {
+                    let ret = u8::try_from(ret & 255).unwrap_or(1);
+                    return Ok(ExitCode::from(ret));
+                } else {
+                    return Err(Error::Signal(rc));
                 }
             }
         }
@@ -122,14 +94,14 @@ impl StatusLine {
         }
     }
 
-    fn clear(&self) -> io::Result<()> {
+    fn clear(&self) -> Result<(), Error> {
         let mut err = self.err.lock();
-        err.write_all(b"\r\x1B[K")?;
-        err.flush()?;
+        err.write_all(b"\r\x1B[K").map_err(Error::Write)?;
+        err.flush().map_err(Error::Write)?;
         Ok(())
     }
 
-    fn print(&self) -> io::Result<()> {
+    fn print(&self) -> Result<(), Error> {
         let elapsed = self.start.elapsed();
         let mut secs = elapsed.as_secs();
         let hours = secs / 3600;
@@ -138,8 +110,8 @@ impl StatusLine {
         secs %= 60;
         let s = format!("Elapsed: {hours:02}:{minutes:02}:{secs:02}");
         let mut err = self.err.lock();
-        err.write_all(s.as_bytes())?;
-        err.flush()?;
+        err.write_all(s.as_bytes()).map_err(Error::Write)?;
+        err.flush().map_err(Error::Write)?;
         Ok(())
     }
 }
@@ -213,5 +185,27 @@ impl<R: AsyncRead + Unpin> Future for NextLine<'_, R> {
                 }
             }
         }
+    }
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("failed to spawn child process")]
+    Spawn(io::Error),
+    #[error(transparent)]
+    Write(io::Error),
+    #[error("error reading from child process's stdout: {0}")]
+    ReadStdout(io::Error),
+    #[error("error reading from child process's stderr: {0}")]
+    ReadStderr(io::Error),
+    #[error("error waiting for child process to terminate: {0}")]
+    Wait(io::Error),
+    #[error("child process killed by signal: {0}")]
+    Signal(ExitStatus),
+}
+
+impl Error {
+    fn is_epipe_write(&self) -> bool {
+        matches!(self, Error::Write(e) if e.kind() == io::ErrorKind::BrokenPipe)
     }
 }
