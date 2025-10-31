48432137dffe21b4682c27d940588f311a4ef3e2
John T. Wodder II <git@varonathe.org>
2025-10-31 16:55:48 -0400

    Use a dedicated future for reading lines of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use anyhow::Context;
+use anyhow::Context as _;
+use std::future::Future;
 use std::io::{self, Write};
+use std::pin::{pin, Pin};
 use std::process::{ExitCode, Stdio};
+use std::task::{ready, Context, Poll};
 use std::time::{Duration, Instant};
 use tokio::{
-    io::{AsyncBufReadExt, BufReader},
+    io::{AsyncRead, ReadBuf},
     process::Command,
     time::interval,
 };
+
+const READ_BUFFER_SIZE: usize = 2048;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<ExitCode> {
@@ -25,12 +30,8 @@ async fn main() -> anyhow::Result<ExitCode> {
         .kill_on_drop(true)
         .spawn()
         .context("failed to start command")?;
-    let mut stdout = BufReader::new(p.stdout.take().expect("Child.stdout should be Some"));
-    let mut outbuf = Vec::new();
-    let mut outeof = false;
-    let mut stderr = BufReader::new(p.stderr.take().expect("Child.stderr should be Some"));
-    let mut errbuf = Vec::new();
-    let mut erreof = false;
+    let mut stdout = ByteLines::new(p.stdout.take().expect("Child.stdout should be Some"));
+    let mut stderr = ByteLines::new(p.stderr.take().expect("Child.stderr should be Some"));
     print_elapsed(start)?;
     loop {
         tokio::select! {
@@ -38,30 +39,26 @@ async fn main() -> anyhow::Result<ExitCode> {
                 clear_elapsed_line()?;
                 print_elapsed(start)?;
             },
-            r = stdout.read_until(b'\n', &mut outbuf), if !outeof => {
+            r = stdout.next_line() => {
                 match r {
-                    Ok(_) if !outbuf.is_empty() => {
+                    Ok(line) => {
                         clear_elapsed_line()?;
-                        io::stdout().lock().write_all(&outbuf)?;
+                        io::stdout().lock().write_all(&line)?;
                         print_elapsed(start)?;
-                        outbuf.clear();
                     }
-                    Ok(_) => outeof = true,
                     Err(e) => {
                         clear_elapsed_line()?;
                         return Err(e).context("error reading from process's stdout");
                     }
                 }
             }
-            r = stderr.read_until(b'\n', &mut errbuf), if !erreof => {
+            r = stderr.next_line() => {
                 match r {
-                    Ok(_) if !errbuf.is_empty() => {
+                    Ok(line) => {
                         clear_elapsed_line()?;
-                        io::stderr().lock().write_all(&errbuf)?;
+                        io::stderr().lock().write_all(&line)?;
                         print_elapsed(start)?;
-                        errbuf.clear();
                     }
-                    Ok(_) => erreof = true,
                     Err(e) => {
                         clear_elapsed_line()?;
                         return Err(e).context("error reading from process's stderr");
@@ -109,4 +106,76 @@ fn print_elapsed(start: Instant) -> io::Result<()> {
     err.write_all(s.as_bytes())?;
     err.flush()?;
     Ok(())
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct ByteLines<R> {
+    reader: R,
+    buffer: Vec<u8>,
+    next_index: usize,
+    eof: bool,
+}
+
+impl<R> ByteLines<R> {
+    fn new(reader: R) -> Self {
+        ByteLines {
+            reader,
+            buffer: Vec::new(),
+            next_index: 0,
+            eof: false,
+        }
+    }
+
+    fn get_line(&mut self) -> Option<Vec<u8>> {
+        if let Some(i) = self.buffer[self.next_index..]
+            .iter()
+            .position(|&b| b == b'\n')
+        {
+            let newline_pos = self.next_index + i;
+            self.next_index = 0;
+            Some(self.buffer.drain(0..=newline_pos).collect())
+        } else if self.eof {
+            (!self.buffer.is_empty()).then(|| std::mem::take(&mut self.buffer))
+        } else {
+            self.next_index = self.buffer.len();
+            None
+        }
+    }
+
+    fn next_line<'a>(&'a mut self) -> NextLine<'a, R> {
+        NextLine { inner: self }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct NextLine<'a, R> {
+    inner: &'a mut ByteLines<R>,
+}
+
+impl<R: AsyncRead + Unpin> Future for NextLine<'_, R> {
+    type Output = io::Result<Vec<u8>>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        loop {
+            if let Some(ln) = self.inner.get_line() {
+                return Poll::Ready(Ok(ln));
+            } else if self.inner.eof {
+                return Poll::Pending;
+            } else {
+                let mut buf0 = vec![0u8; READ_BUFFER_SIZE];
+                let mut buf = ReadBuf::new(&mut buf0);
+                let reader = pin!(&mut self.inner.reader);
+                match ready!(reader.poll_read(cx, &mut buf)) {
+                    Ok(()) => {
+                        if buf.filled().is_empty() {
+                            self.inner.eof = true;
+                        } else {
+                            self.inner.buffer.extend_from_slice(buf.filled());
+                        }
+                    }
+                    Err(e) => return Err(e).into(),
+                }
+            }
+        }
+    }
 }
